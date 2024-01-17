c5404b534f13b5869b247c9ccd5f945bf4e63560
LeeByoungWook <lbw3973@naver.com>
2024-01-18 00:29:36 +0900

    conf: tailwind Config screens 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,12 @@
 export default {
   content: ["./src/**/*.{js,ts,tsx,jsx}"],
   theme: {
+    screens: {
+      xs: "320px",
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+    },
     extend: {},
   },
   plugins: [],
