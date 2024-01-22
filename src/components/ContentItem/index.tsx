82dc2c2e1c97e8e99051d8162b2d8169032b7e30
LeeByoungWook <lbw3973@naver.com>
2024-01-22 23:10:01 +0900

    feat: 카테고리 및 태그 클릭시 검색 페이지로 navigate

--- src/components/ContentItem/index.tsx
+++ src/components/ContentItem/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+import { useNavigate } from "react-router-dom";
 
 interface Content {
   channelName: string;
@@ -12,7 +13,9 @@ interface Content {
 const ContentItem = ({ content }: { content: Content }) => {
   const [isClicked, setIsClicked] = useState(false);
   const [hasOpenButton, setHasOpenButton] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
   const contentBlockRef = useRef<HTMLDivElement>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (contentBlockRef.current?.clientHeight && contentBlockRef.current?.clientHeight >= 240) {
@@ -29,13 +32,14 @@ const ContentItem = ({ content }: { content: Content }) => {
         </div>
         <div className="flex justify-between items-center gap-4 pr-2">
           <img src="https://picsum.photos/66/66" alt="Server_Icon" className="w-[66px] h-[66px]" />
-          <div className="flex flex-col gap-2 justify-start items-start w-full whitespace-nowrap">
+          <div className="flex flex-col gap-2 justify-start items-start w-full whitespace-nowrap py-1">
             <span className="bg-[#424549] rounded-md px-1">{content.category}</span>
             <div className="flex gap-2 flex-wrap">
               {content.tag.map((tag, index) => (
                 <span
                   key={index}
                   className="bg-[#dbeafe] rounded-md px-1 text-black font-bold cursor-pointer hover:bg-[#7bcace] duration-300"
+                  onClick={() => navigate(`/search/${tag}`)}
                 >
                   {tag}
                 </span>
@@ -55,10 +59,23 @@ const ContentItem = ({ content }: { content: Content }) => {
           </div>
           {hasOpenButton && (
             <div
-              className={`absolute bottom-0 left-1/2 -translate-x-1/2 text-center w-full h-16 rounded-b-md bg-gradient-to-b from-transparent to-black`}
+              className={`absolute bottom-0 left-1/2 -translate-x-1/2 text-center w-full h-16 rounded-b-md bg-gradient-to-b from-transparent to-black cursor-pointer`}
+              onClick={() => setIsClicked(!isClicked)}
+              onMouseOver={() => setIsHovered(true)}
+              onMouseOut={() => setIsHovered(false)}
             >
-              <button className={`pt-5 ${isClicked && "pb-5"}`} onClick={() => setIsClicked(!isClicked)}>
-                {isClicked ? <SlArrowUp size={30} /> : <SlArrowDown size={30} />}
+              <button className={`pt-5 ${isClicked && "pb-5"} ${isHovered && "translate-y-1"} duration-300`}>
+                {isClicked ? (
+                  isHovered ? (
+                    <SlArrowUp size={30} color="#0099ff" />
+                  ) : (
+                    <SlArrowUp size={30} />
+                  )
+                ) : isHovered ? (
+                  <SlArrowDown size={30} color="#0099ff" />
+                ) : (
+                  <SlArrowDown size={30} />
+                )}
               </button>
             </div>
           )}
--- src/components/SidaBar/index.tsx
+++ src/components/SidaBar/index.tsx
@@ -1,4 +1,7 @@
+import { getTags } from "@/apis/server";
 import category from "@/constants/category.json";
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
 
 const mock_tag = [
   "게임(99)",
@@ -7,16 +10,22 @@ const mock_tag = [
   "롤(96)",
   "소통(95)",
   "리그오브레전드(94)",
+  "백엔드(92)",
   "배틀그라운드(44)",
   "발로란트(33)",
   "스팀게임(32)",
+  "프론트엔드(31)",
   "새벽(23)",
   "대화(9)",
 ];
 
 const SideBar = () => {
+  const { data } = useQuery({ queryKey: ["tags"], queryFn: getTags });
+  console.log(data);
+
+  const navigate = useNavigate();
   return (
-    <div className="relative md:max-w-[300px] md:min-w-[250px] pt-3 max-w-[50%] min-w-[350px]">
+    <aside className="relative md:max-w-[300px] md:min-w-[250px] pt-3 max-w-[50%] min-w-[350px]">
       <div className="w-[90%] bg-[#7289da] p-3 text-white font-Pretendard rounded-lg sticky top-40 mx-auto">
         <div>
           <h3 className="text-xl text-[#424242] text-center mb-3 after:block after:w-1/5 after:h-px after:bg-[rgba(0,0,0,0.2)] after:mx-auto">
@@ -27,6 +36,7 @@ const SideBar = () => {
               <li
                 key={index}
                 className="bg-[#424549] py-1 px-2 rounded-md font-bold cursor-pointer text-sm text-center"
+                onClick={() => navigate(`/search/category/${item}`)}
               >
                 {item}
               </li>
@@ -40,14 +50,18 @@ const SideBar = () => {
           </h3>
           <ul className="flex gap-2 flex-wrap">
             {mock_tag.map((item, index) => (
-              <li key={index} className="bg-blue-100 text-black py-1 px-2 rounded-md font-bold cursor-pointer text-sm">
+              <li
+                key={index}
+                className="bg-blue-100 text-black py-1 px-2 rounded-md font-bold cursor-pointer text-sm"
+                onClick={() => navigate(`/search/tag/${item}`)}
+              >
                 {item}
               </li>
             ))}
           </ul>
         </div>
       </div>
-    </div>
+    </aside>
   );
 };
 
