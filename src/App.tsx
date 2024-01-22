a992461345da50f37fa4a0e30abee17ca2214523
LeeByoungWook <lbw3973@naver.com>
2024-01-22 23:06:38 +0900

    feat: ReactQuery Provider 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import ReactQueryProvider from "./components/Common/ReactQueryProvider";
 import Router from "./routes/Router";
 function App() {
   return (
-    <>
+    <ReactQueryProvider>
       <Router />
-    </>
+    </ReactQueryProvider>
   );
 }
 
