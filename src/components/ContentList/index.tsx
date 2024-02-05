ff99048a9a07f798aa16333ccfecec8ee24f64ea
LeeByoungWook <lbw3973@naver.com>
2024-02-06 02:13:15 +0900

    feat: fetch loading gif 추가

--- src/components/ContentList/index.tsx
+++ src/components/ContentList/index.tsx
@@ -4,15 +4,24 @@ import { getBoards } from "@/apis/server";
 import { IContent } from "@/types/server";
 
 const ContentList = ({ searchType, searchParam }: { searchType?: string; searchParam?: string }) => {
-  const { data: boards } = useQuery<IContent[]>({
+  const { data: boards, isLoading } = useQuery<IContent[]>({
     queryKey: ["boards", searchType, searchParam],
     queryFn: () => getBoards(searchType, searchParam),
   });
 
+  console.log(isLoading);
+
   return (
-    <div className="flex flex-wrap mx-auto px-4 font-Pretendard justify-center md:justify-normal w-full">
-      {boards && boards.map((content, index) => <ContentItem content={content} key={index} hasJoinButton />)}
-    </div>
+    <>
+      {isLoading && (
+        <div className="fixed flex justify-center items-center top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)] ">
+          <img src="/loading.gif" alt="loading" className="mx-auto" />
+        </div>
+      )}
+      <div className="flex flex-wrap mx-auto px-4 font-Pretendard justify-center md:justify-normal w-full">
+        {boards && boards.map((content, index) => <ContentItem content={content} key={index} hasJoinButton />)}
+      </div>
+    </>
   );
 };
 
