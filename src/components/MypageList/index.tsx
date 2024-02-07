9082a6fd1d6aa7a8a9b5ad5ff0f8babc02b55d67
LeeByoungWook <lbw3973@naver.com>
2024-02-07 17:28:36 +0900

    feat: 마이페이지 내 게시글 불러오기 구현

--- src/components/MypageList/index.tsx
+++ src/components/MypageList/index.tsx
@@ -8,10 +8,12 @@ import { useGetUserInfo } from "@/hooks/useGetUserInfo";
 
 const MypageList = () => {
   const { userInfo } = useGetUserInfo();
-  const { data: boards, isLoading } = useQuery<IContent>({
+  const { data: myBoards, isLoading } = useQuery<IContent>({
     queryKey: ["myboards"],
     queryFn: () => getMyBoards(userInfo?.id as string),
   });
+
+  console.log(myBoards);
   return (
     <>
       {isLoading && (
@@ -21,17 +23,15 @@ const MypageList = () => {
       )}
       <div className="w-[1200px] flex flex-col gap-10">
         <div>
-          <div className="border-b-2 border-solid ">
-            <h1 className="text-3xl font-bold p-3">추가한 내 서버</h1>
-            <div className="flex flex-wrap mx-auto px-4 font-Pretendard justify-center md:justify-normal w-full">
-              {boards &&
-                boards.list.map((content, index) => <ContentItem content={content} key={index} hasJoinButton />)}
+          <h1 className="border-b-2 border-solid text-3xl font-bold p-3">추가한 내 서버</h1>
+          <div className="flex flex-wrap mx-auto px-4 font-Pretendard justify-center md:justify-normal w-full">
+            {myBoards &&
+              myBoards.list.map((content, index) => <ContentItem content={content} key={index} hasJoinButton />)}
+            <div className="flex">
+              <button className="w-[180px] h-14 bg-[#7289da] rounded-lg text-2xl mt-5">
+                <NavLink to={"/new"}>+ 서버 추가</NavLink>
+              </button>
             </div>
-          </div>
-          <div className="flex ">
-            <button className="w-[180px] h-14 bg-[#7289da] rounded-lg text-2xl mt-5">
-              <NavLink to={"/new"}>+ 서버 추가</NavLink>
-            </button>
           </div>
         </div>
 
