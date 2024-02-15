684ad5dc95be686cde66962025a8c3718cd0a880
SedanLee <water8048@naver.com>
2024-02-15 13:05:27 +0900

    feat: 마이페이지 문구 추가

--- src/components/MypageList/index.tsx
+++ src/components/MypageList/index.tsx
@@ -30,11 +30,16 @@ const MypageList = () => {
       )}
       <div className="w-[1200px] flex flex-col gap-10">
         <div>
-          <h1 className="border-b-2 border-solid text-3xl font-bold p-3">추가한 내 서버</h1>
+          <h1 className="flex border-b-2 border-solid text-3xl font-bold p-3 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-nowrap">
+            내 서버
+            <h2 className="text-xs flex items-end text-wrap pl-3">
+              * 서버의 수정을 원하신다면, 서버 추가를 재등록하면 됩니다.
+            </h2>
+          </h1>
           <div className="flex flex-wrap mx-auto px-4 font-Pretendard justify-center md:justify-normal w-full">
             {myBoards && myBoards.list.map((content, index) => <ContentItem content={content} key={index} />)}
             <div className="flex">
-              <button className="w-[180px] h-14 bg-[#7289da] rounded-lg text-2xl mt-5">
+              <button className="w-[180px] h-14 bg-[#3d9148] rounded-lg text-2xl mt-5">
                 <NavLink to={"/new"}>+ 서버 추가</NavLink>
               </button>
             </div>
